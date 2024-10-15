4a99263d318f0dcb61b8cfd1b9992eda2eb9fed6
和紀 井上 <k024g1045@g.neec.ac.jp>
2024-10-15 10:56:45 +0900

    main関数内の文字変更

--- PM1 git_3.cpp
+++ PM1 git_3.cpp
@@ -5,7 +5,8 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";]
+    std::cout << "Hello PullRequest branch changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
